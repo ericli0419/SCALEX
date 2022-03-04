7716df0c717723f0464f198f0928f9acb3dac428
Yuzhe Li <liyuzhe0419@outlook.com>
2022-03-04 12:24:42 +0800

    Add files via upload

---
+++ scalex/__init__.py
@@ -0,0 +1,8 @@
+# from pkg_resources import get_distribution
+
+# __version__ = get_distribution('scalex').version
+__version__ = '0.2.0'
+__author__ = 'Lei Xiong'
+__email__ = 'jsxlei@gmail.com'
+
+from .function import SCALEX, label_transfer
